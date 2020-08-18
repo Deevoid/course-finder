2d1078d337fc1432bb6f1d07e7f2a339459c0ccb
Deevoid <dev.4.pandey@gmail.com>
2020-08-18 10:42:57 +0530

    added dynamic data to cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./components/Card";
+import axios from "axios";
+import Moment from "react-moment";
 
 function App() {
+  const [course, setCourse] = useState();
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/https://frontend-hiring.appspot.com/all_courses`,
+        {
+          params: {
+            secret: "HelloMars",
+          },
+        },
+        {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+          },
+        }
+      )
+      .then((res) => {
+        console.log(JSON.parse(res.data.payload));
+        setCourse(JSON.parse(res.data.payload));
+      });
+  }, []);
+
   return (
     <div className="App">
       <header>Course Finder</header>
-      <Card />
+      {course &&
+        course.map((item, index) => {
+          return (
+            <Card
+              key={index}
+              title={item.title}
+              instructor={item.instructor_name}
+              description={item.description}
+              registration={
+                new Date() < item.start_date
+                  ? `Pre Registration`
+                  : item.start_date < new Date() && new Date() < item.end_date
+                  ? `Ongoing`
+                  : `Completed`
+              }
+              start={<Moment format="MMMM D">{item.start_date}</Moment>}
+              end={<Moment format="MMMM D">{item.end_date}</Moment>}
+              duration={
+                <Moment diff={item.start_date} unit="weeks">
+                  {item.end_date}
+                </Moment>
+              }
+              workload={item.estimated_workload}
+            />
+          );
+        })}
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,26 +2,34 @@ import React from "react";
 import DateRangeIcon from "@material-ui/icons/DateRange";
 import ErrorOutlineIcon from "@material-ui/icons/ErrorOutline";
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div className="card">
       <div className="card-header">
         <div className="avatar" />
         <div className="info">
-          <p className="title">Course 1</p>
-          <p className="instructor">Prof 1</p>
+          <p className="title">{props.title}</p>
+          <p className="instructor">{props.instructor}</p>
         </div>
       </div>
       <div className="card-body">
         <ErrorOutlineIcon />
-        <p className="description">!Lorem asdf asdf</p>
+        <p
+          className="description"
+          dangerouslySetInnerHTML={{ __html: props.description }}
+        ></p>
       </div>
       <div className="card-footer">
         <DateRangeIcon />
         <div className="footer-info">
-          <p className="registration">Reg</p>
-          <p className="full-date">Full date</p>
-          <p className="duration">Duration</p>
+          <p className="registration">{props.registration}</p>
+          <div className="full-date">
+            <span>{props.start}</span> - <span>{props.end}</span>
+          </div>
+          <div className="duration">
+            <span>{props.duration}</span> week course,
+            <span>{props.workload}</span>
+          </div>
         </div>
       </div>
     </div>
