022629beecab7317b8335c7f9f8d23a3fd81f0a7
Deevoid <dev.4.pandey@gmail.com>
2020-08-18 11:06:01 +0530

    added styling to the cards

--- src/App.js
+++ src/App.js
@@ -31,32 +31,41 @@ function App() {
   return (
     <div className="App">
       <header>Course Finder</header>
-      {course &&
-        course.map((item, index) => {
-          return (
-            <Card
-              key={index}
-              title={item.title}
-              instructor={item.instructor_name}
-              description={item.description}
-              registration={
-                new Date() < item.start_date
-                  ? `Pre Registration`
-                  : item.start_date < new Date() && new Date() < item.end_date
-                  ? `Ongoing`
-                  : `Completed`
-              }
-              start={<Moment format="MMMM D">{item.start_date}</Moment>}
-              end={<Moment format="MMMM D">{item.end_date}</Moment>}
-              duration={
-                <Moment diff={item.start_date} unit="weeks">
-                  {item.end_date}
-                </Moment>
-              }
-              workload={item.estimated_workload}
-            />
-          );
-        })}
+      <div className="container">
+        <p style={{ margin: "0 0 10px 0", fontWeight: "500", color: "grey" }}>
+          {course && course.length} courses open for registration
+        </p>
+
+        <div className="grid">
+          {course &&
+            course.map((item, index) => {
+              return (
+                <Card
+                  key={index}
+                  title={item.title}
+                  instructor={item.instructor_name}
+                  description={item.description}
+                  registration={
+                    new Date() < item.start_date
+                      ? `Pre Registration`
+                      : item.start_date < new Date() &&
+                        new Date() < item.end_date
+                      ? `Ongoing`
+                      : `Completed`
+                  }
+                  start={<Moment format="MMMM D">{item.start_date}</Moment>}
+                  end={<Moment format="MMMM D">{item.end_date}</Moment>}
+                  duration={
+                    <Moment diff={item.start_date} unit="weeks">
+                      {item.end_date}
+                    </Moment>
+                  }
+                  workload={item.estimated_workload}
+                />
+              );
+            })}
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -9,25 +9,32 @@ export default function Card(props) {
         <div className="avatar" />
         <div className="info">
           <p className="title">{props.title}</p>
-          <p className="instructor">{props.instructor}</p>
+          <h5 style={{ margin: "6px 0 0 0" }} className="instructor">
+            {props.instructor}
+          </h5>
         </div>
       </div>
       <div className="card-body">
-        <ErrorOutlineIcon />
+        <ErrorOutlineIcon style={{ marginLeft: "0.5rem", color: "grey" }} />
         <p
           className="description"
           dangerouslySetInnerHTML={{ __html: props.description }}
         ></p>
       </div>
       <div className="card-footer">
-        <DateRangeIcon />
+        <DateRangeIcon style={{ marginLeft: "0.5rem", color: "grey" }} />
         <div className="footer-info">
-          <p className="registration">{props.registration}</p>
+          <h5
+            style={{ margin: "0 0 2px 0", color: "grey" }}
+            className="registration"
+          >
+            {props.registration}
+          </h5>
           <div className="full-date">
             <span>{props.start}</span> - <span>{props.end}</span>
           </div>
           <div className="duration">
-            <span>{props.duration}</span> week course,
+            <span>{props.duration}</span> week course,{" "}
             <span>{props.workload}</span>
           </div>
         </div>
